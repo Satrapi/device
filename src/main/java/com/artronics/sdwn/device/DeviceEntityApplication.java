5d7561ff93198eb93e34adfdf41ddf0e8622f708
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2016-01-07 01:40:07 +0100

    initial commit

---
+++ src/main/java/com/artronics/sdwn/device/DeviceEntityApplication.java
@@ -0,0 +1,12 @@
+package com.artronics.sdwn.device;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceEntityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceEntityApplication.class, args);
+	}
+}
---
+++ src/test/java/com/artronics/sdwn/device/DeviceEntityApplicationTests.java
@@ -0,0 +1,18 @@
+package com.artronics.sdwn.device;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DeviceEntityApplication.class)
+@WebAppConfiguration
+public class DeviceEntityApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
