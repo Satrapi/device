c53bee4b53ba51b4f257730188f5dd6e2fa7c52c
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2016-01-15 04:20:13 +0100

    change model. neighbor has no longer bidir rel with node. packet has no device because it has already rel with nodes. change session manager to create a session if there is no active one

--- src/main/java/com/artronics/sdwn/device/controller/DeviceControllerImpl.java
+++ src/main/java/com/artronics/sdwn/device/controller/DeviceControllerImpl.java
@@ -1,5 +1,6 @@
 package com.artronics.sdwn.device.controller;
 
+import com.artronics.sdwn.controller.address.NodeAddressResolver;
 import com.artronics.sdwn.controller.services.PacketService;
 import com.artronics.sdwn.domain.entities.packet.PacketEntity;
 import org.apache.log4j.Logger;
@@ -11,25 +12,28 @@ public class DeviceControllerImpl implements DeviceController
 {
     private final static Logger log = Logger.getLogger(DeviceControllerImpl.class);
 
-//    private NodeAddressResolver addressResolver;
+    private NodeAddressResolver addressResolver;
 
     private PacketService packetService;
 
     @Override
     public void addPacket(PacketEntity packet)
     {
-//        packet = addressResolver.resolveNodeAddress(packet);
+        packet = addressResolver.resolveNodeAddress(packet);
 
-        packetService.addPacket(packet);
+        try {
+            packetService.addPacket(packet);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-//    @Autowired
-//    public void setAddressResolver(
-//            NodeAddressResolver addressResolver)
-//    {
-//        this.addressResolver = addressResolver;
-//    }
-
+    @Autowired
+    public void setAddressResolver(
+            NodeAddressResolver addressResolver)
+    {
+        this.addressResolver = addressResolver;
+    }
 
     @Autowired
     public void setPacketService(PacketService packetService)
