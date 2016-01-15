47af9a24024b2321243b99c482a1fcc7e689f5b3
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2016-01-15 14:24:00 +0100

    change packet service

--- src/main/java/com/artronics/sdwn/device/controller/UnicastSdwnNodeAddressResolverImpl.java
+++ src/main/java/com/artronics/sdwn/device/controller/UnicastSdwnNodeAddressResolverImpl.java
@@ -3,7 +3,9 @@ package com.artronics.sdwn.device.controller;
 import com.artronics.sdwn.controller.address.NodeAddressResolver;
 import com.artronics.sdwn.controller.remote.NodeRegistrationService;
 import com.artronics.sdwn.domain.entities.DeviceConnectionEntity;
+import com.artronics.sdwn.domain.entities.node.SdwnNeighbor;
 import com.artronics.sdwn.domain.entities.node.SdwnNodeEntity;
+import com.artronics.sdwn.domain.entities.packet.Packet;
 import com.artronics.sdwn.domain.entities.packet.PacketEntity;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 @Component
 public class UnicastSdwnNodeAddressResolverImpl implements NodeAddressResolver
@@ -37,6 +40,32 @@ public class UnicastSdwnNodeAddressResolverImpl implements NodeAddressResolver
     @Override
     public PacketEntity resolveNodeAddress(PacketEntity packet)
     {
+        registerSrcAndDstIfNotExist(packet);
+
+        if (packet.getType().equals(Packet.Type.REPORT)) {
+            registerNeighborsIfNotExist(packet);
+        }
+
+        return packet;
+    }
+
+    private PacketEntity registerNeighborsIfNotExist(PacketEntity packet)
+    {
+        Set<SdwnNeighbor> neighbors = SdwnNeighbor.createNeighbors(packet);
+        neighbors.forEach(neighbor -> {
+            SdwnNodeEntity node = neighbor.getNode();
+            if (!nodesMap.containsKey(node.getAddress())){
+                node.setDevice(device);
+                node = nodeRegistrationService.registerNode(node);
+                nodesMap.put(node.getAddress(),node);
+            }
+        });
+
+        return packet;
+    }
+
+    private PacketEntity registerSrcAndDstIfNotExist(PacketEntity packet)
+    {
         SdwnNodeEntity srcNode = packet.getSrcNode();
         SdwnNodeEntity dstNode = packet.getDstNode();
         srcNode.setDevice(device);
@@ -44,22 +73,21 @@ public class UnicastSdwnNodeAddressResolverImpl implements NodeAddressResolver
 
         if (!nodesMap.containsKey(srcNode.getAddress())) {
             srcNode = nodeRegistrationService.registerNode(packet.getSrcNode());
-            nodesMap.put(srcNode.getAddress(),srcNode);
-        }
-        else {
+            nodesMap.put(srcNode.getAddress(), srcNode);
+        }else {
             srcNode = nodesMap.get(srcNode.getAddress());
         }
 
-        if (!nodesMap.containsKey(dstNode.getAddress())){
+        if (!nodesMap.containsKey(dstNode.getAddress())) {
             dstNode = nodeRegistrationService.registerNode(packet.getDstNode());
-            nodesMap.put(dstNode.getAddress(),dstNode);
-        }
-        else {
+            nodesMap.put(dstNode.getAddress(), dstNode);
+        }else {
             dstNode = nodesMap.get(dstNode.getAddress());
         }
 
         packet.setSrcNode(srcNode);
         packet.setDstNode(dstNode);
+
 
         return packet;
     }
